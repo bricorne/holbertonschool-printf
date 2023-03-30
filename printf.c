ec033866ceb63f2be98dd403d221350ba19b34c7
bricorne <bricepoitevineau@gmail.com>
2023-03-30 07:24:53 -0700

    Refactoring function

--- printf.c
+++ printf.c
@@ -4,97 +4,42 @@
 
 int _printf(const char *const format, ...)
 {
-    int i = 0;
-    int b = 0;
-    int len = 0;
-    int j = 0;
-    va_list args;
-
-    operateurs ops[] = {
-        {'s', print_string},
-        {'c', print_char},
-        {0, NULL}
-    };
-
-    va_start(args, format);
-    while (format[i] != '\0')
-    {
-	    if (format[i] == '%')
-	    {
-		i++;
-        	b = 0;
-        while (ops[b].op != '\0')
-        {
-            if (ops[b].op == format[i])
-            {
-              len += ops[b].func(args);
-	      len -= 2;
-	      j++;
-	    }
-            b++;
-	    if (b == 2 && j == 0)
-	    {
-		    if (format[i] != '%')
-		    {
-		    _putchar(format[i-1]);
-		    }
-		    else
-		    {
-			    len--;
-		    }
-		    _putchar(format[i]);
-	    }
-	}
-	    }
-	    else
-	    {
-	_putchar(format[i]);
-	    }
-	i++;
-    }
-    va_end(args);
-    len += i;
-    /*printf("%d\n", len);*/
-    return (len);
-}
-
-
-
-
-
-
-/*int _printf(const char *format, ...)
-{
+	int i = 0, b = 0, len = 0;
 	va_list args;
+	operateurs ops[] = {
+		{'s', print_string},
+		{'c', print_char},
+		{0, NULL}
+	};
 	va_start(args, format);
-	int i;
-
-	for (i = 0; i != '\0'; i++)
+	while (format != NULL && format[i] != '\0')
 	{
-		if (format[i] == "%")
+		if (format[i] == '%')
 		{
-			if (format[i+1] == "c")
+			i++;
+			b = 0;
+			while (ops[b].op != '\0')
 			{
-				print_char(args);
-				i++;
-				args++;
+				if (ops[b].op == format[i])
+				{
+					len += ops[b].func(args);
+					break;
+				}
+
+				b++;
 			}
-			else if (format[i+1] == "s")
+			if (ops[b].op == 0)
 			{
-				print_string(args);
-				i++;
-				args++;
-			}
-			else
-			{
-				_putchar(format[i]);
+				len += _putchar(format[i]);
 			}
 		}
 		else
 		{
-		_putchar(format[i]);
+			len += _putchar(format[i]);
 		}
+		i++;
 	}
 	va_end(args);
-}*/
+	return (len);
+}
 
